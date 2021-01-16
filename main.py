0cbcaf451085b49fa34e233f7a55114801d9b74a
john <john@jpatmore.plus.com>
2021-01-16 23:46:43 +0000

    Initial code. Add row via Sheety working.

---
+++ main.py
@@ -0,0 +1,8 @@
+# This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+
+import sheety
+import pprint
+
+
+# Add a new row to the spreadsheet
+# sheety.post_new_row("Perth", "", 600)
---
+++ sheety.py
@@ -0,0 +1,37 @@
+import os
+import requests
+import datetime as dt
+
+
+# https://dashboard.sheety.co/
+# https://sheety.co/docs/requests
+BEARER = os.environ.get("API_Bearer_Sheety")
+USERNAME = os.environ.get("API_Username_Sheety")
+PROJECT = "flightDeals"
+SHEET = "prices"
+
+
+def post_new_row(city, iata_code, lowest_price):
+
+    base_url = "https://api.sheety.co"
+    endpoint_url = f"/{USERNAME}/{PROJECT}/{SHEET}"
+    url = base_url + endpoint_url
+
+    headers = {
+        "Authorization": f"Bearer {BEARER}",
+        "Content-Type": "application/json",
+    }
+
+    # column names are made camelCase, e.g. "Lowest Price" becomes "lowestPrice"
+    body = {
+        "price": {
+            "city": city,
+            "iataCode": iata_code,
+            "lowestPrice": lowest_price,
+        }
+    }
+
+    response = requests.post(url=url, headers=headers, json=body)
+    response.raise_for_status()
+    print(response.text)
+
